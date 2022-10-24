b2376a66d3a37a0b588a17e19fc5607b36002df0
Hriday Keswani <hridaykeswani@gmail.com>
2022-10-24 16:54:05 +0530

    implement calcualtion of outputs in batches

--- neural_network_from_scratch/main.py
+++ neural_network_from_scratch/main.py
@@ -1,9 +1,13 @@
 from audioop import bias
 import numpy as np
 
-inputs = [1.2, 5.1, 2.1]
-weights = [[3.1, 2.1, 8.7], [0.5, -0.91, -0.5], [-0.26, -0.27, 0.17]]
+inputs = [[1.2, 5.1, 2.1, 1.6],
+            [2.0, 5.0, -1.0, 2.0],
+            [-1.5, -0.27, 0.17, 0.87]]
+weights = [[3.1, 2.1, 8.7, -0.35], 
+            [0.5, -0.91, -0.5, 1.1], 
+            [-0.26, -0.27, 0.17, 4.3]]
 biases = [3, 2, 0.5]
 
-output = np.dot(weights, inputs) + biases
+output = np.dot(np.array(inputs), np.array(weights).T) + biases
 print(output)
